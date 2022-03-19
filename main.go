f8742e99f5c0a7835c574a8f7dc82079557034d8
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 21:20:47 +0900

    slice and array

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main // main -> only for compiling
 import "fmt"
 
 func main() {
-	a := 2
-	b := &a
-	*b = 20
-	fmt.Println(a)
+	names := []string{"nico", "lynn", "dal"} // no number in definition -> slice
+	names = append(names, "flynn")
+	fmt.Println(names)
 }
