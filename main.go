78114e56b22be1832a29b05566de9e76771731c6
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 21:28:47 +0900

    struct

--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main // main -> only for compiling
 
 import "fmt"
 
+type person struct {
+	name    string
+	age     int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string{"name": "nico", "age": "12"}
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
+	favFood := []string{"kimchi", "ramen"}
+	nico := person{name: "nico", age: 18, favFood: favFood}
 	fmt.Println(nico)
 }
