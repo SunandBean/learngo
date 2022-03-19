a90395da84387438e89c78c1313d73de0e842acf
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 14:57:08 +0900

    add other package

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main // main -> only for compiling
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/sunandbean/learngo/something"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	something.SayHello() // Start with Uppercase -> export from other package
+	// something.sayBye()   // Start with Lowercase -> private function
 }
