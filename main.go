99c1223a356f3aecb6d451627744bc24c9bba2e2
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 21:14:02 +0900

    pointer

--- main.go
+++ main.go
@@ -2,33 +2,8 @@ package main // main -> only for compiling
 
 import "fmt"
 
-func superAdd(numbers ...int) int {
-	total := 0
-	for _, number := range numbers {
-		total += number
-	}
-	return total
-}
-
-func canIDrink(age int) bool {
-	if koreanAge := age + 2; koreanAge < 18 {
-		return false
-	}
-	return true
-}
-
-func canIDrink2(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	case 50:
-		return false
-	}
-	return false
-}
-
 func main() {
-	fmt.Println(canIDrink2(18))
+	a := 2
+	b := &a
+	fmt.Println(&a, b)
 }
