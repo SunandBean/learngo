803a43b94c4aa7563da95c5b86f94571f83f6ca3
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 15:14:38 +0900

    multiple argument in functino

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
 func main() {
-	totalLength, upperName := lenAndUpper("nico")
-	fmt.Println(totalLength, upperName)
+	repeatMe("nico", "lynn", "dal", "marl", "flynn")
 }
