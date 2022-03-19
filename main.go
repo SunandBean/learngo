2830272eedc4ce56f956b237b6499f0fb3ae4ffd
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 15:01:05 +0900

    variable, shorten variable"

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main // main -> only for compiling
 import "fmt"
 
 func main() {
-	const name string = "nico"
-	name = "Lynn"
+	name := "nico" // == var name string = "nico", go will automatically find the type.
+	name = "lynn"
 	fmt.Println(name)
 }
