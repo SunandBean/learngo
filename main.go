577378a52ea184dee8c9600d49a238b802db0613
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 22:39:28 +0900

    handle error

--- main.go
+++ main.go
@@ -3,11 +3,17 @@ package main // main -> only for compiling
 import (
 	"fmt"
 	"learngo/accounts"
+	"log"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
 	fmt.Println(account)
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(account.Balance())
 }
