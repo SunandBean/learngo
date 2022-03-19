b1cf293679862acc069ddcdf0e83550b3ffead6d
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 15:21:22 +0900

    defer

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func lenAndUpper(name string) (int, string) {
 }
 
 func lenAndUpper2(name string) (length int, uppercase string) {
+	defer fmt.Println("I'm done") // defer: running this command after finishing this function
 	length = len(name)
 	uppercase = strings.ToUpper(name)
 	return
@@ -21,6 +22,6 @@ func repeatMe(words ...string) {
 
 func main() {
 	repeatMe("nico", "lynn", "dal", "marl", "flynn")
-	totalLength, upperCase := lenAndUpper("nico")
+	totalLength, upperCase := lenAndUpper2("nico")
 	fmt.Println(totalLength, upperCase)
 }
