44673973153845e069ed7f66b110ac3e7e0ad6dc
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 15:05:06 +0900

    add comment about variable

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main // main -> only for compiling
 import "fmt"
 
 func main() {
-	name := "nico" // == var name string = "nico", go will automatically find the type.
+	name := "nico" // == var name string = "nico", go will automatically find the type, only can use in function
 	name = "lynn"
 	fmt.Println(name)
 }
