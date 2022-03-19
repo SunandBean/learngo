fb8150e57398480e8066c1ed9e31edbe40ed5759
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 15:19:24 +0900

    naked return

--- main.go
+++ main.go
@@ -9,10 +9,18 @@ func lenAndUpper(name string) (int, string) {
 	return len(name), strings.ToUpper(name)
 }
 
+func lenAndUpper2(name string) (length int, uppercase string) {
+	length = len(name)
+	uppercase = strings.ToUpper(name)
+	return
+}
+
 func repeatMe(words ...string) {
 	fmt.Println(words)
 }
 
 func main() {
 	repeatMe("nico", "lynn", "dal", "marl", "flynn")
+	totalLength, upperCase := lenAndUpper("nico")
+	fmt.Println(totalLength, upperCase)
 }
