abf4c92d664ddb0c197f63910ab38aa62441f26f
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-19 15:09:28 +0900

    function - type definition is important for compiler

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main // main -> only for compiling
 
 import "fmt"
 
+func multiply(a int, b int) int {
+	return a * b
+}
+
+func multiply2(a, b int) int {
+	return a * b
+}
+
 func main() {
-	name := "nico" // == var name string = "nico", go will automatically find the type, only can use in function
-	name = "lynn"
-	fmt.Println(name)
+	fmt.Println(multiply(2, 2))
+	fmt.Println(multiply2(2, 2))
 }
