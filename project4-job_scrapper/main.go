1a35866a1ec40c9d7e95dfc77c48e38cc3d3fef5
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-20 12:34:38 +0900

    extract jobs but blocked by something

--- project4-job_scrapper/main.go
+++ project4-job_scrapper/main.go
@@ -5,9 +5,18 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type extractedJob struct {
+	id       string
+	title    string
+	location string
+	salary   string
+	summary  string
+}
 
 // var baseURL string = "https://kr.indeed.com/jobs?=q=python&limit=50"
 
@@ -15,17 +24,54 @@ var baseURL string = "https://kr.indeed.com/jobs?=q=python"
 var LastpageURL string = "https://kr.indeed.com/jobs?q=python&limit=50&start=9999"
 
 func main() {
+	var jobs []extractedJob
 	totalPages := getPages()
 	fmt.Println(totalPages)
 
 	for i := 0; i < totalPages; i++ {
-		getPage(i)
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
 	}
+
+	fmt.Println(jobs)
 }
 
-func getPage(page int) {
+func getPage(page int) []extractedJob {
+	var jobs []extractedJob
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	// fmt.Println(id, title, location, salary, summary)
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
 }
 
 //baseURL 참조 변경 및 페이지 추출 함수
@@ -34,37 +80,27 @@ func getPages() int {
 	res, err := http.Get(LastpageURL)
 	checkErr(err)
 	checkCode(res)
+	fmt.Println("Check LastpageURL")
+	fmt.Println(err, res.StatusCode)
 
 	defer res.Body.Close()
+
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
+	fmt.Println("New Document")
+	fmt.Println(err, doc)
 
 	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		fmt.Println("Find page")
 		pages, err = strconv.Atoi(s.Find("b").Text())
+		fmt.Println(pages, err)
 	})
 	return pages
 }
 
-// func getPages() int {
-// 	pages := 0
-
-// 	res, err := http.Get(baseURL)
-// 	checkErr(err)
-// 	checkCode(res)
-
-// 	defer res.Body.Close()
-
-// 	doc, err := goquery.NewDocumentFromReader(res.Body)
-// 	checkErr(err)
-
-// 	// doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
-// 	// 	// fmt.Println(s.Html())
-// 	// 	// fmt.Println(s.Find("a").Length())
-// 	// 	pages = s.Find("a").Length()
-// 	// })
-
-// 	return pages
-// }
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
 
 func checkErr(err error) {
 	if err != nil {
