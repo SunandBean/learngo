c3ff0ce26ab66dbee34763dda60c7f064698af8a
sunandbean <sunbin.vincent.kim@gmail.com>
2022-03-20 13:09:58 +0900

    writing job, but not works

--- project4-job_scrapper/main.go
+++ project4-job_scrapper/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -33,7 +35,28 @@ func main() {
 		jobs = append(jobs, extractedJobs...)
 	}
 
-	fmt.Println(jobs)
+	// fmt.Println(jobs)
+	writeJobs(jobs)
+	fmt.Println("Done, extracted", len(jobs))
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	defer w.Flush()
+
+	headers := []string{"ID", "Title", "Location", "Salary", "Summary"}
+
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range jobs {
+		jobSlice := []string{job.id, job.title, job.location, job.salary, job.summary}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
 }
 
 func getPage(page int) []extractedJob {
@@ -43,12 +66,16 @@ func getPage(page int) []extractedJob {
 	res, err := http.Get(pageURL)
 	checkErr(err)
 	checkCode(res)
+	fmt.Println(err, res.StatusCode)
+
 	defer res.Body.Close()
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
+	fmt.Println(err, doc)
 
-	searchCards := doc.Find(".jobsearch-SerpJobCard")
+	// searchCards := doc.Find(".jobsearch-SerpJobCard")
+	searchCards := doc.Find(".tapItem")
 
 	searchCards.Each(func(i int, card *goquery.Selection) {
 		job := extractJob(card)
@@ -60,11 +87,17 @@ func getPage(page int) []extractedJob {
 
 func extractJob(card *goquery.Selection) extractedJob {
 	id, _ := card.Attr("data-jk")
-	title := cleanString(card.Find(".title>a").Text())
-	location := cleanString(card.Find(".sjcl").Text())
-	salary := cleanString(card.Find(".salaryText").Text())
-	summary := cleanString(card.Find(".summary").Text())
-	// fmt.Println(id, title, location, salary, summary)
+	// title := cleanString(card.Find(".title>a").Text())
+	// location := cleanString(card.Find(".sjcl").Text())
+	// salary := cleanString(card.Find(".salaryText").Text())
+	// summary := cleanString(card.Find(".summary").Text())
+
+	title := cleanString(card.Find("h2>span").Text())
+	// title := cleanString(card.Find(".jobTitle").Text())
+	location := cleanString(card.Find(".companyLocation").Text())
+	salary := cleanString(card.Find(".salary-snippet").Text())
+	summary := cleanString(card.Find(".job-snippet").Text())
+	fmt.Println(id, title, location, salary, summary)
 	return extractedJob{
 		id:       id,
 		title:    title,
